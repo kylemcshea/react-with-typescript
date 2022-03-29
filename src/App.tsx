344f7da799ce180312bc7f453df04c5207687189
kylemcshea <kmcshea@stevens.edu>
2022-03-29 13:10:56 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List'
+import AddToList from './components/AddToList';
+
+// useState stores data inside of a component.
+// Typescript allows you to see type errors during development in code.
+
+export interface IState {
+  people: {
+    name:string,
+    age:number,
+    url:string,
+    note?:string
+  }[]
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Lebron James",
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLob1v4T_RAK4ixnA_yoV1mGRCBQdrENfoA&usqp=CAU",
+      age: 36,
+      note: 'The actual GOAT.'
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my Party</h1>
+      <List people = {people}/>
+      <AddToList people = {people} setPeople = {setPeople}/>
     </div>
   );
 }
